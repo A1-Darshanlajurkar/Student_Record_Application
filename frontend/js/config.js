5a67060cf4bd88c3ec6bcf35df6c7045b6c842fd
Shrutika_Dautpure <shrutidautpure2005@gmail.com>
2026-04-30 14:40:35 +0530

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,3 +1,3 @@
 // Replace this URL with your Azure Web App URL after deployment
 // e.g. "https://student-api-app.azurewebsites.net"
-const API_BASE = "const API_BASE = "https://studentrecord-webapp-backend-dpcjafc5cxbebpbz.centralus-01.azurewebsites.net";
+const API_BASE = "const API_BASE = "studentrecord-webapp-backend-dpcjafc5cxbebpbz.centralus-01.azurewebsites.net";
